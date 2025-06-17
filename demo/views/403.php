259dd417f384e2c0e2ced9a5976dc76f2d57a83a
Mpumelelo Moale <moalempumi5@gmail.com>
2025-06-17 21:31:28 +0200

    testing second commit

--- demo/views/403.php
+++ demo/views/403.php
@@ -5,7 +5,7 @@
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <h1 class="text-2xl font-bold">Sorry. You are not authorized to view this page.</h1>
 
-        <p class="mt-4">
+        <p class="mt-4 ">
             <a href="/websites/demo/index.php" class="text-blue-500 underline">Go back home.</a>
         </p>
     </div>
