b272618f90d02166d0ce60584a05e1c6ca133839
Mpumelelo Moale <moalempumi5@gmail.com>
2025-07-28 21:48:36 +0200

    first insert into the database

--- demo/controller/note-create.php
+++ demo/controller/note-create.php
@@ -1,10 +1,17 @@
 <?php 
 require 'functions.php';
+require 'Database.php';
+
+$config = require 'config.php';
+$db = new Database($config['database']);
 
 $heading = 'Create a note';
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    dd($_POST);
+    $db->query('INSERT INTO notes(body, user_id) VALUE(:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => 1
+    ]);
 }
 
 
